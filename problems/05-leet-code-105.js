92e99c2ebd22bc22def75f9ada904a1014ae65e1
Ondre <ondre.williams.77@gmail.com>
2021-03-02 11:00:18 -0800

    start bonus

--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -4,5 +4,14 @@
 const { TreeNode } = require('./01_tree_node.js');
 
 function buildTree(preorder, inorder) {
+    let tree = [preorder.shift()];
+    let index = inorder.indexOf(3);
 
+    let left = inorder.slice(0, index);
+    let right = inorder.slice(index + 1);
+
+    if(left.length){
+        tree.push(left.slice());
+
+    }
 }
