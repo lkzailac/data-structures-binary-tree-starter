825425f6e21d76693475468725f28e769cd9b12f
Ondre <ondre.williams.77@gmail.com>
2021-03-02 12:44:05 -0800

    bonus

--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -4,14 +4,21 @@
 const { TreeNode } = require('./01_tree_node.js');
 
 function buildTree(preorder, inorder) {
-    let tree = [preorder.shift()];
-    let index = inorder.indexOf(3);
+    if(!preorder.length && !inorder.length)
+        return null;
 
-    let left = inorder.slice(0, index);
-    let right = inorder.slice(index + 1);
+    let rootValue = preorder[0];
+    let root = new TreeNode(rootValue);
+    let index = inorder.indexOf(rootValue);
 
-    if(left.length){
-        tree.push(left.slice());
+    let leftInorder = inorder.slice(0, index);
+    let rightInorder = inorder.slice(index + 1);
 
-    }
+    let leftPreorder = preorder.filter(val => leftInorder.includes(val));
+    let rightPreorder = preorder.filter(val => rightInorder.includes(val));
+
+    root.left = buildTree(leftPreorder, leftInorder);
+    root.right = buildTree(rightPreorder, rightInorder);
+
+    return root;
 }
